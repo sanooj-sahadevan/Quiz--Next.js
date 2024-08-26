8ad554fecec8c287ff4801de9bc3cfddfa6a4ab3
sanooj <146158357+sanooj-sahadevan@users.noreply.github.com>
2024-08-26 21:17:03 +0530

    addd

--- quiz/app/page.tsx
+++ quiz/app/page.tsx
@@ -1,26 +1,32 @@
+"use client"; // Add this directive at the top
 
-import DropdownOptions from '@/components/DropDownsOptions'
-import InputBox from '@/components/inputBOx'
-import Button from '@/components/Buttons/page'
+
+import DropdownOptions from '@/components/DropDownsOptions';
+import InputBox from '@/components/inputBOx';
+import Button from '@/components/Buttons/page';
+import { useQuizConfig } from '@/store';
+
 export default function Home() {
+  const quizConfig = useQuizConfig((state: any) => state.config);
+  const addNumberOfQuestions = useQuizConfig(
+    (state: any) => state.addNumberOfQuestions
+  );
 
   return (
-   <section className='flex flex-col justify-center items-center my-10 '>
-{/* //from flowbite */}
-<h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-white md:text-5xl lg:text-6xl dark:text-white">
-  Quiz
-</h1>
-<section className="p-10 my-10 rounded-lg border-2 border-white shadow-xl w-[65%]">
-
-{/* Flowbite? */}
-<InputBox/>
-
-<DropdownOptions/>
-<div className=" flex items-center justify-center">
-<Button/>
-</div>
-
-</section>
-   </section>
-  )
+    <section className="flex flex-col justify-center items-center my-10">
+      <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-white md:text-5xl lg:text-6xl dark:text-white">
+        Quiz
+      </h1>
+      <section className="p-10 my-10 rounded-lg border-2 border-white shadow-xl w-[65%]">
+        <InputBox />
+        <DropdownOptions />
+        <div className="flex items-center justify-center">
+          {quizConfig?.numberOfQuestion > 0 &&
+            quizConfig?.level &&
+            quizConfig?.category?.id &&
+            quizConfig?.type && <Button />}
+        </div>
+      </section>
+    </section>
+  );
 }
